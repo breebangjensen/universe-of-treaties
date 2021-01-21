7e280951142d26cd0f51695bae743cd548efe4c9
breebangjensen <breebangjensen@gmail.com>
2021-01-21 13:47:11 -0800

    Create scrape_treaty_catalogue.py
    Wireframe WIP, don't review yet

---
+++ scrape_treaty_catalogue.py
@@ -0,0 +1,8 @@
+#Objective of script
+#The goal of this script is to pick up where the script get_treaty_catalogue.py leaves off and to scrape through those treaty urls to harvest treaty specific metadata and the URLs
+#for each volume and for each treaty txt or pdf file
+
+#packages
+
+#next steps
+#The output for this script will be used in a third script that will grab the text from all the text document URLS. 
