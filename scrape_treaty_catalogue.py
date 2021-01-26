96b789105bfd1a34111e896e6141ae1357785559
breebangjensen <breebangjensen@gmail.com>
2021-01-26 12:07:54 -0800

    Update scrape_treaty_catalogue.py

--- scrape_treaty_catalogue.py
+++ scrape_treaty_catalogue.py
@@ -8,11 +8,18 @@
 
 #packages to import
 import bs4
+import requests
 
 #read in output from get_treaty_catalogue.py. Output is a .csv file. will be multiple tables so actually need to loop thru them.
 ## need to create set of treaties that have already been scraped, so we don't keep scraping the same ones. 
 # need to put the file name below in a loop
-file_object("data/treaty_catalog_' + str(search_year) + '.csv'", 
+for i in range(1946:2019):  
+            file_object("data/treaty_catalog_' + str(i) + '.csv'")
+ #this is not an connected or functional loop 
+
+#need to grab url from href
+   
+
 
 #Use .csv file to find and open first URL. Tell program which column to find the URL in. Column is called "href". 
             def open_page(url):
