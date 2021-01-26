b38074210fe30e57bdf81c2bb1983ed19c502649
Jose Hernandez <jmhernandeze@gmail.com>
2021-01-26 14:03:06 -0800

    tested miner method but def slower see seperate functions

--- treaty_extract.py
+++ treaty_extract.py
@@ -4,26 +4,53 @@ import re
 import sys
 import itertools
 
-from bs4 import BeautifulSoup
-from requests_html import HTMLSession
 import PyPDF2
 
-def convert_to_txt(pdf_file):
+from io import StringIO
+
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+
+import time 
+
+def convert_to_txt_pypdf(pdf_file):
     pdf_reader = PyPDF2.PdfFileReader(pdf_file)
     txt_file = os.path.splitext(pdf_file)[0] +'.txt'
     with open (txt_file,'w', encoding="utf-8") as pdf_output:
         for page in range(pdf_reader.getNumPages()):
             data = pdf_reader.getPage(page).extractText()
-            data_lines = data.splitlines()
-            data_string = ' '.join([str(elem) for elem in data_lines])
-            pdf_output.write(data_string)
+            pdf_output.write(data)
     with open(txt_file, 'r') as pdf_content:
         pdf_content.read().replace('\n', ' ')
 
-data_path = '/Users/josehernandez/Documents/eScience/projects/universe-of-treaties/data/'
+# Adapted from https://pdfminersix.readthedocs.io/en/latest/tutorial/composable.html
+def convert_to_txt_miner(pdf_path):
+    txt_file = os.path.splitext(file_path)[0] +'.txt'
+    output_string = StringIO()
+    with open(pdf_path, 'rb') as in_file:
+        parser = PDFParser(in_file)
+        doc = PDFDocument(parser)
+        rsrcmgr = PDFResourceManager()
+        device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.create_pages(doc):
+            interpreter.process_page(page)
+        text = output_string.getvalue()
+    with open (txt_file,'w', encoding="utf-8") as pdf_output:
+        pdf_output.write(text)
 
-for pdf in os.listdir(data_path):
-    convert_to_txt(data_path+pdf)
+file_path = '/Users/josehernandez/Documents/eScience/projects/universe-of-treaties/data/v1.pdf'
 
-# test regex
-# begins with [No. #####] d{5}
+t = time.time() 
+convert_to_txt_pypdf(file_path)
+time.time() - t
+# 5.05s
+
+t = time.time() 
+convert_to_txt_miner(file_path)
+time.time() - t
+# 18.38s!! Yikes! 
