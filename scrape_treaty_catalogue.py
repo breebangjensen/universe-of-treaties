b1116f822fc00a7a4bc23c21c633bbd7e302f83f
breebangjensen <breebangjensen@gmail.com>
2021-01-28 13:14:18 -0800

    Update scrape_treaty_catalogue.py

--- scrape_treaty_catalogue.py
+++ scrape_treaty_catalogue.py
@@ -1,7 +1,3 @@
-#Objective of script
-#The goal of this script is to pick up where the script get_treaty_catalogue.py leaves off and to scrape through those treaty urls to harvest treaty specific metadata and the URLs
-#for each volume and for each treaty .txt or .pdf file.
-#packages to import
 
 from bs4 import BeautifulSoup
 import requests
@@ -36,18 +32,14 @@ for table in tables:
                           "Map(s)", "Corrigendum/Addendum", "Participant", "Action", "Date of Notification/Deposit",
                           "Date of Effect"]
             row_data.append(cols)
-        print(row_data)
+            print(row_data)
  
 # save output (will be .csv). try to save one line of csv at a time so the data doesn't get lost if the job crashes. 
 csv_file_name = 'treaty_links'
 out_csv_file = open(csv_file_name, 'w', newline='')
 csv_writer = csv.writer(out_csv_file, delimiter=',')
 _ = csv_writer.writerow(column_names)
+_ = csv_writer.writerow(row_data)
 
 #close url? close csv? 
 out_csv_file.close()
-
-
-
-#next steps
-#The output for this script will be used in a third script that will grab the text from all the text document URLS. 
