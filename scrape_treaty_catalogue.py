e683642aaa1e968784f72b350f392e7ba82e5e33
breebangjensen <breebangjensen@gmail.com>
2021-02-02 08:56:29 -0800

    Update scrape_treaty_catalogue.py
    working on loops

--- scrape_treaty_catalogue.py
+++ scrape_treaty_catalogue.py
@@ -1,39 +1,60 @@
 
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 28 15:25:02 2021
+
+@author: Bree
+"""
+##
 from bs4 import BeautifulSoup
 import requests
 import csv
+import pandas
 
 
+##read columns and file
+col_list = ["href", "title", "reg_num", "reg_date", "type", "con_date", "vol"]
+df = pandas.read_csv("treaty_catalog_1947.csv", usecols=col_list)
+urls=(df["href"])
 
-#read in output from get_treaty_catalogue.py. Output is a .csv file. will be multiple tables so actually need to loop thru them.
-## need to create set of treaties that have already been scraped, so we don't keep scraping the same ones. 
-# need to put the file name below in a loop
-# Currently, no loops
-
-url = "https://treaties.un.org/Pages/showDetails.aspx?objid=0800000280165965&clang=_en"
-#Use .csv file to find and open first URL. Tell program which column to find the URL in. Column is called "href". 
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'html.parser')
+for url in urls: 
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+##attempt to fix overwriting issue, does not fix
+    return _=csv_writer.writerow(row_to_write)
 
 
 # Instantiate CSV
 # save output (will be .csv). try to save one line of csv at a time so the data doesn't get lost if the job crashes. 
-csv_file_name = 'treaty_links.csv'
-out_csv_file = open(csv_file_name, 'w', newline='')
-csv_writer = csv.writer(out_csv_file, delimiter=',')
-column_names=["Registration Number", "Volume in PDF"]
+    csv_file_name = 'treaty_links_2.csv'
+    out_csv_file = open(csv_file_name, 'w', newline='')
+    csv_writer = csv.writer(out_csv_file, delimiter=',')
+    column_names=["reg_num", "title", "reg_year", "type_treaty", "pdf"]
+    _ = csv_writer.writerow(column_names)
 # then write the column names into the csv
 
 # Scrape valuable data
 ## registration number (appears as <span id="lblRegNum1">20</span>)
-reg = soup.find("span", {"id": "lblRegNum1"}).text
+    reg = soup.find("span", {"id": "lblRegNum1"}).text
+
+##title
+    title=soup.find("span", {"id": "lblTitle1"}).text
+
+#registration date
+    reg_year=soup.find("span", {"id": "lblRegDate1"}).text
+
+
+#type
+    type_treaty =soup.find("span", {"id": "lblAgreementType1"}).text
 
 ## pdf path
-pdf = soup.find("a", {"id": "0"})["href"]
+##possible problem--getting multiple pdfs
+    pdf = soup.find("a", {"id": "0"})["href"]
 
-row_to_write = [reg, pdf]
 
-csv_writer.writerow(row_to_write)
+    row_to_write = [reg, title, reg_year, type_treaty, pdf]
+
+   
 
 
 
