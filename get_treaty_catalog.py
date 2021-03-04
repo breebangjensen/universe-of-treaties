563d0c5879295206c53f0d00940fa2cac5d53dba
Spencer Wood <spwood@uw.edu>
2021-03-04 14:23:47 -0800

    Wrap web requests in a try statement to handle flaky internet
    also increase the sleep time on the result pagination
    since that appears to be the most common place for errors

--- get_treaty_catalog.py
+++ get_treaty_catalog.py
@@ -76,92 +76,102 @@ for y,m in month_year_iter(1, 1998, 12, 2000):
     column_names = ['href', 'title', 'reg_num', 'reg_date', 'type', 'con_date', 'vol']
     tid = 'ctl00_ctl00_ContentPlaceHolder1_ContentPlaceHolderInnerPage_dgTreaty'
     url = 'https://treaties.un.org/Pages/AdvanceSearch.aspx?tab=UNTS&clang=_en'
-
-    # Open browser
-    chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument("--headless")
-    browser = webdriver.Chrome(options=chrome_options)
-    browser.get(url)
-    time.sleep(2)
-
-    # Select Treaty filter
-    select = Select(browser.find_element_by_name('ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolderInnerPage$drpSearchObj'))
-    select.select_by_visible_text('Treaty')
-    time.sleep(np.random.randint(4,8))
-
-    ## Select Show Only Original Agreements
-    browser.find_element_by_id('ctl00_ctl00_ContentPlaceHolder1_ContentPlaceHolderInnerPage_rdbtreaty_2').send_keys(Keys.SPACE)
-    select = Select(browser.find_element_by_name('ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolderInnerPage$drpAttribute'))
-    time.sleep(np.random.randint(4,8))
-    #time.sleep(np.random.randint(10, 15))
-
-    ## Select Date of Registration filter
-    select = Select(browser.find_element_by_name('ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolderInnerPage$drpAttribute'))
-    time.sleep(np.random.randint(4,8))
-    select.select_by_visible_text('Date of Registration')
-
-    # Select Date Range filter
-    # to date first, then from date.  order is important.
-    to_field = browser.find_element_by_name('ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolderInnerPage$txtTo')
-    time.sleep(2)
-    to_field.click()
-    time.sleep(2)
-    to_field.clear()
-    time.sleep(2)
-    to_field.send_keys(to_date)
-    from_field = browser.find_element_by_name('ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolderInnerPage$txtFrom')
-    from_field.click()
-    time.sleep(2)
-    from_field.clear()
-    time.sleep(2)
-    from_field.send_keys(from_date)
-    time.sleep(2)
-    # then add the query to the search criteria
-    browser.find_element_by_name('ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolderInnerPage$btnAdd').send_keys(Keys.SPACE)
-    time.sleep(2)
-
-    # Do the Search
-    browser.find_element_by_name('ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolderInnerPage$btnSubmit').send_keys(Keys.SPACE)
-    time.sleep(8)
-
-    # QAQC
-    record_count_elem = browser.find_element_by_id('ctl00_ctl00_ContentPlaceHolder1_ContentPlaceHolderInnerPage_lblMsg').text
-    record_count = record_count_elem.split(':')[1].strip()
-    print('This query returned ' + record_count + ' treaties.')
-    if int(record_count) > 500:
-        raise ValueError('Search results are capped at 500 records.  Try smaller date range.')
-
-    # Iterate and save data
-    csv_file_path = 'data/' + csv_file_name
-    out_csv_file = open(csv_file_path, 'w', newline='')
-    csv_writer = csv.writer(out_csv_file, delimiter=',')
-    _ = csv_writer.writerow(column_names)
-
-    i = 1
+    
+    # Search and scrape UN Treaties webpages
+    # everything is nested in a try statement to handle flaky internet
     while True:
-        tbody = browser.find_element_by_id(tid).find_element_by_tag_name('tbody')
-        tr_list = tbody.find_elements_by_tag_name('tr')
-        d = len(tr_list[1].find_elements_by_tag_name('td'))
-        if d < 10:
-            s = '0%d'%d
-        else:
-            s = str(d)
-        id_ = 'ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolderInnerPage$dgTreaty$ctl01$ctl%s'%(s)
-        btn = WebDriverWait(browser,
-                            np.random.randint(5,7)).until(EC.presence_of_element_located((By.NAME, id_)))
-        for csv_row in parse_page(tr_list[3:-2]):
-            _ = csv_writer.writerow(csv_row)
-        print('Parsing page %d of approx %d pages from %s...'%(i, int(record_count)/10, date_range))
-        if btn.get_attribute('disabled') is not None:
-            break
-        btn.click()
-        i += 1
-        time.sleep(np.random.randint(7, 14))
+        try:
+            # Open browser
+            chrome_options = webdriver.ChromeOptions()
+            # chrome_options.add_argument("--headless")
+            browser = webdriver.Chrome(options=chrome_options)
+            browser.get(url)
 
-    # Close connections
-    out_csv_file.close()
-    browser.close()
+            # Select Treaty filter
+            select = Select(browser.find_element_by_name('ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolderInnerPage$drpSearchObj'))
+            select.select_by_visible_text('Treaty')
+            time.sleep(np.random.randint(4,8))
 
+            # Select Show Only Original Agreements
+            browser.find_element_by_id('ctl00_ctl00_ContentPlaceHolder1_ContentPlaceHolderInnerPage_rdbtreaty_2').send_keys(Keys.SPACE)
+            select = Select(browser.find_element_by_name('ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolderInnerPage$drpAttribute'))
+            time.sleep(np.random.randint(4,8))
+            #time.sleep(np.random.randint(10, 15))
+
+            # Select Date of Registration filter
+            select = Select(browser.find_element_by_name('ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolderInnerPage$drpAttribute'))
+            time.sleep(np.random.randint(4,8))
+            select.select_by_visible_text('Date of Registration')
+
+            # Select Date Range filter
+            # to date first, then from date.  order is important.
+            to_field = browser.find_element_by_name('ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolderInnerPage$txtTo')
+            time.sleep(2)
+            to_field.click()
+            time.sleep(2)
+            to_field.clear()
+            time.sleep(2)
+            to_field.send_keys(to_date)
+            from_field = browser.find_element_by_name('ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolderInnerPage$txtFrom')
+            from_field.click()
+            time.sleep(2)
+            from_field.clear()
+            time.sleep(2)
+            from_field.send_keys(from_date)
+            time.sleep(2)
+            # then add the query to the search criteria
+            browser.find_element_by_name('ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolderInnerPage$btnAdd').send_keys(Keys.SPACE)
+            time.sleep(2)
+
+            # Do the Search
+            browser.find_element_by_name('ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolderInnerPage$btnSubmit').send_keys(Keys.SPACE)
+            time.sleep(8)
+
+            # QAQC
+            record_count_elem = browser.find_element_by_id('ctl00_ctl00_ContentPlaceHolder1_ContentPlaceHolderInnerPage_lblMsg').text
+            record_count = record_count_elem.split(':')[1].strip()
+            print('This query returned ' + record_count + ' treaties.')
+            if int(record_count) > 500:
+                raise ValueError('Search results are capped at 500 records.  Try smaller date range.')
+
+            # Write to a csv
+            csv_file_path = 'data/' + csv_file_name
+            out_csv_file = open(csv_file_path, 'w', newline='')
+            csv_writer = csv.writer(out_csv_file, delimiter=',')
+            _ = csv_writer.writerow(column_names)
+
+            # Iterate over pages and save results
+            i = 1
+            while True:
+                tbody = browser.find_element_by_id(tid).find_element_by_tag_name('tbody')
+                tr_list = tbody.find_elements_by_tag_name('tr')
+                d = len(tr_list[1].find_elements_by_tag_name('td'))
+                if d < 10:
+                    s = '0%d'%d
+                else:
+                    s = str(d)
+                id_ = 'ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolderInnerPage$dgTreaty$ctl01$ctl%s'%(s)
+                btn = WebDriverWait(browser,
+                                    np.random.randint(5,7)).until(EC.presence_of_element_located((By.NAME, id_)))
+                for csv_row in parse_page(tr_list[3:-2]):
+                    _ = csv_writer.writerow(csv_row)
+                print('Parsing page %d of approx %d pages from %s...'%(i, int(record_count)/10, date_range))
+                if btn.get_attribute('disabled') is not None:
+                    break
+                btn.click()
+                i += 1
+                time.sleep(np.random.randint(20, 25))
+
+        # Handle exception by retrying the month
+        except:
+            print('Failed to retrieve paged results.  Retrying...')
+            browser.close()
+            continue
+
+        # Or celebrate success by moving to next month
+        out_csv_file.close()
+        browser.close()
+        break
 
 
 # browser.find_elements_by_partial_link_text('showDetails')
